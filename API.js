a5334987136dd9f3493b861121ae5cfa135096a9
Sangjun Lee <mato268@naver.com>
2023-12-29 11:46:46 +0900

    ADD JS [API.js]

--- API.js
+++ API.js
@@ -4,69 +4,51 @@ const ul = document.getElementById("ul");
 
 const API_KEY =
   "test_efb42ba6f037a09fa65e28125f10993901f1b04a6a72fa38874476df6649b33021322271e4aa69d519c1f1c287a7bf59";
-const characterName = "토끼헌터"
-const nicknameUrl =
-  "https://open.api.nexon.com/fconline/v1/id?nickname=" + characterName;
+
+function request(path) {
+  return fetch(`https://open.api.nexon.com/fconline/v1${path}`, {
+    headers: {
+      "x-nxopen-api-key": API_KEY,
+    },
+  });
+}
 
 // 계정 식별자(ouid) 조회
-const getUserOuid = fetch(nicknameUrl, {
-  headers: {
-    "x-nxopen-api-key": API_KEY,
-  },
-})
-  .then(response => response.json())
-  .then(myJson => {
-    const userOuid = myJson.ouid.toString();
-    console.log(userOuid);
-    console.log(myJson);
-  })
-  .catch(error => console.error(error));
+function getUserOuid(characterName) {
+  return request(`/id?nickname=` + characterName)
+    .then(response => response.json())
+    .then(data => data.ouid)
+    .then(ouid => {
+      // 기본 정보 조회
+      return request("/user/basic?ouid=" + ouid);
+    })
+    .then(response => response.json())
+    .catch(console.error);
+}
 
-/*const searchParams = new URLSearchParams(nicknameUrl);
-searchParams.toString();
-for (const [key, value] of searchParams) {
-  console.log(`${key}=${value}`);
-}*/
+function userList() {
+  getUserOuid(text.value).then(({ level, nickname }) => {
+    const li = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "삭제";
 
-/*const url = new URL(nicknameUrl);
-url.search = new URLSearchParams(characterName)
-url.toString();
-console.log(url)*/
+    li.innerText = `레벨: ${level} 닉네임: ${nickname}`;
+    ul.appendChild(li);
+    li.append(deleteButton);
 
-const ouidUrl = "https://open.api.nexon.com/fconline/v1/user/basic?ouid=" + "2766f6264e2181be64cb394f4eb4c374";
-
-// 기본 정보 조회
-const getUserNameAndLevel = fetch(ouidUrl, {
-  headers: {
-    "x-nxopen-api-key": API_KEY,
-  },
-})
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+    deleteButton.addEventListener("click", () => {
+      ul.removeChild(li);
+    });
+  });
+}
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  if(!text.value){
-    alert('다시 입력하세요.')
+  if (!text.value) {
+    alert("다시 입력하세요.");
     return;
   }
-  console.log(text.value);
 
   userList(text.value);
   text.value = "";
-})
-
-function userList() {
-  const li = document.createElement('li');
-  const deleteButton = document.createElement(('button'));
-  deleteButton.textContent = "삭제";
-
-  li.innerText = text.value;
-  ul.appendChild(li);
-  li.append(deleteButton);
-
-  deleteButton.addEventListener("click", () => {
-    ul.removeChild(li);
-  })
-}
+});
